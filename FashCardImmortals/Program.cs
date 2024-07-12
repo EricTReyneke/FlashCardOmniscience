48e9809a34578dacb436eddd99fe5b4be6f278ef
Eric Theodore Reyneke <91662607+EricTReyneke@users.noreply.github.com>
2024-07-12 21:58:09 +0200

    Initial commit

---
+++ FashCardImmortals/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+#region Injections
+
+#endregion
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,2 @@
+# FashCardImmortals
+ Developing a sophisticated flashcard application designed to enhance learning efficiency. This project is built with ASP.NET and adheres to SOLID principles for robust, maintainable code.
